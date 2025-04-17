9c475180b85aeb4be8536ee97ffc1ffdc1a0e665
Edoardo Torrini <edoardo.torrini@gmail.com>
2025-04-18 01:28:28 +0200

    update .gitignore to exclude test directory + python package and add requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh: 
+	description = fh.read() 
+
+setup( 
+	name="pyv2x", 
+	version="0.0.1", 
+	author="Edoardo Torrini", 
+	author_email="edoardo.torrini@gmail.com", 
+	packages=[
+		"pyv2x",
+		"pyv2x.v2x_utils",
+		"pyv2x.v2x_network"
+	], 
+	description="Python package to communicate using v2x", 
+	long_description=description, 
+	long_description_content_type="text/markdown", 
+	url="https://github.com/EdoardoTorrini/pyv2x", 
+	license='MIT', 
+	python_requires='>=3.8', 
+	install_requires=[],
+	include_package_data=True,
+	package_data={
+		"pyv2x": ["etsi_its_header.asn"]
+	}
+) 
