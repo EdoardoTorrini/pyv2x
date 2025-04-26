eb34ef0b0528687d3012835253674df2bd0cba6e
Edoardo Torrini <edoardo.torrini@gmail.com>
2025-04-26 11:25:08 +0200

    add v2x_cam for cam msg sending

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
 	packages=[
 		"pyv2x",
 		"pyv2x.v2x_utils",
-		"pyv2x.v2x_network"
+		"pyv2x.v2x_network",
+		"pyv2x.v2x_cam"
 	], 
 	description="Python package to communicate using v2x", 
 	long_description=description, 
