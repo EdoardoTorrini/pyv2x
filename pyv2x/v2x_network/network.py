e717fd15c1fad31994776d17062b79d69c5cfa43
Edoardo Torrini <edoardo.torrini@gmail.com>
2025-10-06 16:45:39 +0200

    modify the network to avoid the freeze of the callback

--- pyv2x/v2x_network/network.py
+++ pyv2x/v2x_network/network.py
@@ -1,11 +1,13 @@
 from scapy.packet import Packet
-from scapy.sendrecv import sendp, sniff
+from scapy.sendrecv import sendp, sniff, AsyncSniffer
 
 from threading import Thread, Semaphore
 from typeguard import typechecked
-from queue import PriorityQueue
+from queue import Queue, Full, Empty
 
 from pyv2x.etsi import ETSI, ETSI_CAM, ETSI_DENM
+
+import time
 
 
 required = [ "interface" ]
@@ -13,34 +15,45 @@ required = [ "interface" ]
 @typechecked
 class V2xNetwork:
 
-    _queue = PriorityQueue(maxsize=0)
-
-
     def __init__(self, **kwargs):
 
         if not all(item in kwargs.keys() for item in required):
             raise Exception(f"wrong definition of V2xNetwork: {required}")
         
         self.__dict__.update(kwargs)
-        tshark = Thread(target=self.start_listener_v2x, daemon=True).start()
+
+        self._queue = Queue(maxsize=100)
+        self._tshark = AsyncSniffer(iface=self.interface, prn=self.callback, store=False, monitor=True)
+        self._tshark.start()
+        # Thread(target=self.start_v2x_listener, daemon=True).start()
+
+    # def start_v2x_listener(self):
+    #     while True:
+    #         self._tshark = AsyncSniffer(iface=self.interface, prn=self.callback, store=False, monitor=True)
+    #         self._tshark.start()
+    #         time.sleep(1)
+    #         self._tshark.stop()
+
 
     def send_msg(self, packet: Packet) -> None:
         sendp(packet, iface=self.interface)
-
-    def start_listener_v2x(self):
-        sniff(iface=self.interface, prn=self.callback, count=10)
-
-    def callback(self, packet: Packet):        
-        match ETSI.get_message_id(packet):
-            case 1:
-                self._queue.put((ETSI_DENM, packet))
-            case 2:
-                self._queue.put((ETSI_CAM, packet))
-            case _:
-                pass
+    
+    def callback(self, packet: Packet):
+        try:
+            self._queue.put(packet)
+        except Full:
+            self._queue.get_nowait()
+            self._queue.put_nowait(packet)
+        except Exception as e:
+            raise Exception(f"callback exception: {str(e)}")
     
     def is_empty(self) -> bool:
         return self._queue.empty()
 
-    def get_new_msg(self) -> tuple:
-        return self._queue.get()
+    def get_new_msg(self) -> Packet | None:
+        try:
+            return self._queue.get_nowait()
+        except Empty:
+            return None 
+        except Exception as e:
+            raise Exception(f"get_new_msg execption: {str(e)}")
